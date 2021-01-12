0ce8b73c8500ad559aac33850e4cd6098b5e691c
Evandro oliveira <vandoaparecido@hotmail.com>
2021-01-12 00:33:54 -0300

    Utilizando useState

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Text, View } from 'react-native';
 import {css} from './assets/css/Css';
 
 import Page from './views/Page';
 
 export default function App() {
+
+  const [product, setProduct] = useState('inicial');
 
   const props={
     empresa: 'Desenvolvimento',
@@ -14,6 +16,7 @@ export default function App() {
   return (
     <View style={css.container}>
       <Text>Rastreio</Text>
+      <Text>{product}</Text>
       <Page {...props}/>
     </View>
   );
