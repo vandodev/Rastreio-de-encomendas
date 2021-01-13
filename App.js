c10269589464f35f982fefe2add2a732a6e63fff
Evandro oliveira <vandoaparecido@hotmail.com>
2021-01-13 00:01:46 -0300

    Trabalhando com React Navigation

--- App.js
+++ App.js
@@ -1,33 +1,21 @@
 import React, {useState, useEffect} from 'react';
 import {Text, View, Button,Alert } from 'react-native';
 import {css} from './assets/css/Css';
+import Home from './views/Home';
 
-import Page from './views/Page';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 
 export default function App() {
 
-  const [product, setProduct] = useState('Bola');
-  const [quantify, setQuantify] = useState(0);
-
-  useEffect(() => {
-    if(quantify > 0){
-      Alert.alert('Novo produto adicionado');
-    }
-  },[quantify]);
-
-  const props={
-    empresa: 'Desenvolvimento',
-    name: 'Evandro',
-    produto: product,
-    quantidade: quantify
-  };
+  const Stack = createStackNavigator();
 
   return (
-    <View style={css.container}>
-      <Text>Rastreio</Text>
-      <Text>{product}</Text>
-      <Page {...props}/>
-      <Button title='Adicionar produtos' onPress={() => setQuantify(quantify + 1)}/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
