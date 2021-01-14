74f68c03d1cb8ef27c3391a19875faff4c509486
Evandro oliveira <vandoaparecido@hotmail.com>
2021-01-14 01:20:48 -0300

    Exibindo msg com useState

--- views/Login.js
+++ views/Login.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, TextInput,TouchableOpacity, View, KeyboardAvoidingView,Platform, Image } from 'react-native';
 import {css} from '../assets/css/Css';
 
 export default function Login (){
+
+    const [display, setDisplay] = useState('none');
+
     return(
         <KeyboardAvoidingView
             style={[css.container, css.darkbg]}
@@ -14,14 +17,14 @@ export default function Login (){
             </View>
 
             <View>
-                <Text style={css.login__msg()}>Usuário ou senha inválidos</Text>
+                <Text style={css.login__msg(display)}>Usuário ou senha inválidos</Text>
             </View>
 
             <View style={css.login__form}>
                 <TextInput placeholder='Usuário'  style={css.login__input} />
                 <TextInput placeholder='Senha'  style={css.login__input} secureTextEntry={true}/>
 
-                <TouchableOpacity  style={css.login__button}>
+                <TouchableOpacity  style={css.login__button} onPress={()=> setDisplay('flex')}>
                     <Text  style={css.login__bottonText}>Entrar</Text>
                 </TouchableOpacity>
 
