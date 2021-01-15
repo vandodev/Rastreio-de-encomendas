525105d202a95dc1976d5861133ee0123a32d4d2
Evandro oliveira <vandoaparecido@hotmail.com>
2021-01-15 00:52:28 -0300

    Inserindo dados no banco

--- Controller.js
+++ Controller.js
@@ -1,13 +1,24 @@
 const express=require('express');
 const cors=require('cors');
 const bodyParser=require('body-parser');
+const models = require('./models');
 
 const app=express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/',(req,res)=>{
-    res.send('Meu servidor backend já está rodando!');
+let user = models.User;
+let tracking = models.Tracking;
+let product = models.Product;
+
+app.get('/create',async (req,res)=>{
+    let create=await user.create({
+        name: "novo",
+        password: "123",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    });
+    res.send('Usuário criado com sucesso!');
 });
 
 let port=process.env.PORT || 3000;
