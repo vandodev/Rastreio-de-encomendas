04fc429deb22071bf33670ca981f3286ba33aa3c
Evandro oliveira <vandoaparecido@hotmail.com>
2021-01-17 04:06:53 -0300

    Criando logout do sistema

--- views/arearestrita/Profile.js
+++ views/arearestrita/Profile.js
@@ -2,9 +2,16 @@ import React , {useState, useEffect} from 'react';
 import {Text, View, TouchableOpacity } from 'react-native';
 import {css} from '../../assets/css/Css';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Profile ({navigation}){
-   
+
+    async function logout()
+    {
+        await AsyncStorage.clear();
+        navigation.navigate('Login');
+    }
+
     return(
         <View style={css.area__menu}>
             <TouchableOpacity style={css.button__home2} onPress={()=>props.navigation.navigate('Home')}>
@@ -13,7 +20,7 @@ export default function Profile ({navigation}){
 
             <Text style={css.area__title}>Profile</Text>
 
-            <TouchableOpacity style={css.button__logout}>
+            <TouchableOpacity style={css.button__logout} onPress={()=>logout()}>
                 <Icon name="sign-out" size={20} color="#999" />
             </TouchableOpacity>
 
